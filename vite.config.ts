dcdf2fc8f306b72587539ed9d1d606492632c9cb
mihir-shah-dev <connect@mihirshah.dev>
2025-10-15 22:45:20 -0700

    rolling back the base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
- // base: process.env.VITE_BASE_PATH || "/",
-  base : "/",
+  base: process.env.VITE_BASE_PATH || "/",
+ // base : "/",
   server: {
     host: "::",
     port: 8080,
